8944ab79bb20fa43248a413c531b69e5b91ef78d
Jef Mayer <modeseventyeight@gmail.com>
2020-10-05 21:01:33 -0700

    Adding active states, and setting active menu states on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-console */
 import load from './modules/loader';
 import menu from './modules/menu';
-import sceneNavigation from './modules/scene-navigation';
 
 load();
 menu();
-sceneNavigation();
 /* eslint-enable no-console */
--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -7,6 +7,8 @@ const menu = () => {
   const navHightlight = document.querySelector('.nav-highlight');
   const sectionNavItems = document.querySelectorAll('.nav-menu button');
 
+  let activeMenuItem = '';
+
   const removeActiveClass = () => {
     body.classList.remove(menuAnimatelass);
     setTimeout(() => {
@@ -43,15 +45,68 @@ const menu = () => {
     }
   });
 
+  const sceneNames = [];
+  const sceneNavBtns = document.querySelectorAll('.nav-menu .scene-navigation-btn');
+  for (let i = 0; i < sceneNavBtns.length; i++) { /* eslint-disable-line no-plusplus */
+    sceneNames.push(sceneNavBtns[i].getAttribute('data-scene-name'));
+  }
+  // console.log(sceneNames);
+
+  const setActiveMenuState = (btn) => {
+    navHightlight.style.top = `${btn.offsetTop}px`;
+    navHightlight.classList.add('active');
+  };
+
+  const getSceneOffsetPos = (sceneName) => {
+    const sceneTop = document.querySelector(`.project-animation-${sceneName}`).offsetTop;
+    /* const sceneDetails = document.querySelector(`.project-details-${sceneName}`);
+    let sceneOffset = 0;
+    if (sceneDetails !== null) {
+      sceneOffset = sceneDetails.offsetHeight;
+    } */
+    return sceneTop; // + sceneOffset;
+  };
+
+  window.addEventListener('scroll', () => {
+    // console.log(`ypos: ${window.pageYOffset}`);
+    for (let i = sceneNames.length - 1; i >= 0; i--) { /* eslint-disable-line no-plusplus */
+      if (getSceneOffsetPos(sceneNames[i]) <= window.pageYOffset) {
+        // console.log(`${sceneNames[i]}: ${getSceneOffsetPos(sceneNames[i])}`);
+        activeMenuItem = sceneNames[i];
+        const btn = document.querySelector(`.nav-menu [data-scene-name="${activeMenuItem}"]`);
+        setActiveMenuState(btn);
+        return;
+      }
+    }
+  });
+
+  const scrollToPosition = (pos) => {
+    window.scrollTo({
+      top: pos,
+      behavior: 'smooth',
+    });
+  };
+
   [].map.call(sectionNavItems, btn => (
     btn.addEventListener('mouseover', () => {
-      navHightlight.style.top = `${btn.offsetTop}px`;
-      navHightlight.classList.add('active');
+      setActiveMenuState(btn);
     })
   ));
   [].map.call(sectionNavItems, btn => (
     btn.addEventListener('mouseout', () => {
-      navHightlight.classList.remove('active');
+      if (activeMenuItem !== '') {
+        const activeBtn = document.querySelector(`.nav-menu [data-scene-name="${activeMenuItem}"]`);
+        setActiveMenuState(activeBtn);
+      } else {
+        navHightlight.classList.remove('active');
+      }
+    })
+  ));
+  [].map.call(sectionNavItems, btn => (
+    btn.addEventListener('click', () => {
+      const sceneName = btn.getAttribute('data-scene-name');
+      const pos = getSceneOffsetPos(sceneName);
+      scrollToPosition(pos);
     })
   ));
 };
